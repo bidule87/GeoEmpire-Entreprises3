d1022ce03cf73b53b76f0a0c8071cbad780b79d5
bidule87 <clavetmatt@gmail.com>
2026-03-15 19:29:56 +0100

    Update entreprises.js

--- js/core/modules/entreprises.js
+++ js/core/modules/entreprises.js
@@ -4,7 +4,7 @@ import {
     changerNomEntreprise, 
     changerPhotoEntreprise, 
     calculerValeurEntreprise 
-} from "../entrepriseCore.js";
+} from "./js/core/entrepriseCore.js";
 
 // AJOUT — Import des modules Marketing + Gestion
 import { initMarketing } from "./marketing.js";
